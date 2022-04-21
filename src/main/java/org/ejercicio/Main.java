22c85065278e4cc35488de3131b99b0293951f42
Emiliano Acevedo <emi.acevedo.letras@gmail.com>
2022-04-21 12:48:46 -0300

    end

--- src/main/java/org/ejercicio/Main.java
+++ src/main/java/org/ejercicio/Main.java
@@ -3,7 +3,7 @@ package org.ejercicio;
 public class Main {
     public static void main(String[] args) {
 
-        CocheCRUDImpl cocheCRUD = new CocheCRUDImpl();
+        CocheCRUD cocheCRUD = new CocheCRUDImpl();
         cocheCRUD.save();
         cocheCRUD.findAll();
         cocheCRUD.delete();
